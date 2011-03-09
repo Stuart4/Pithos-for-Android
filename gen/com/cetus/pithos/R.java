b509b1aaa1236eef5731c83b99fec0ac38bb2e89
David Collins <davidcollins4481@gmail.com>
2011-03-09 08:51:34 -0500

    Incremental commit. Support Eclair to Gingerbread.

--- gen/com/cetus/pithos/R.java
+++ gen/com/cetus/pithos/R.java
@@ -14,12 +14,19 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060004;
-        public static final int credentials=0x7f060005;
+        public static final int button1=0x7f06000b;
+        public static final int credentials=0x7f06000c;
         public static final int credentials_label=0x7f060000;
-        public static final int imageView1=0x7f060001;
-        public static final int password=0x7f060003;
-        public static final int username=0x7f060002;
+        public static final int imageView1=0x7f060003;
+        public static final int password=0x7f060009;
+        public static final int pwRow=0x7f060008;
+        public static final int tableLayout1=0x7f060001;
+        public static final int tableRow1=0x7f060002;
+        public static final int tableRow3=0x7f06000a;
+        public static final int textView1=0x7f060005;
+        public static final int unRow=0x7f060006;
+        public static final int username=0x7f060007;
+        public static final int welcomeLabel=0x7f060004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -33,6 +40,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int credentials_label=0x7f040002;
+        public static final int enter_password=0x7f040005;
+        public static final int enter_username=0x7f040004;
         public static final int hello=0x7f040000;
         public static final int pandora_credentials_label=0x7f040003;
     }
