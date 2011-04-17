e99c953190ac12bd5147179429445080fba7fad3
David Collins <davidcollins4481@gmail.com>
2011-04-16 21:15:28 -0400

    Add song item for list. Stations layout fixes

--- gen/com/cetus/pithos/R.java
+++ gen/com/cetus/pithos/R.java
@@ -18,22 +18,26 @@ public final class R {
         public static final int icon=0x7f020004;
     }
     public static final class id {
-        public static final int InnerRelativeLayout=0x7f070007;
-        public static final int about=0x7f070010;
-        public static final int ban=0x7f07000c;
-        public static final int credentials=0x7f07000f;
-        public static final int imageButton1=0x7f070008;
-        public static final int imageButton2=0x7f070009;
-        public static final int imageButton3=0x7f07000a;
+        public static final int InnerRelativeLayout=0x7f07000b;
+        public static final int ScrollView01=0x7f070009;
+        public static final int about=0x7f070014;
+        public static final int ban=0x7f070010;
+        public static final int credentials=0x7f070013;
+        public static final int imageButton1=0x7f07000c;
+        public static final int imageButton2=0x7f07000d;
+        public static final int imageButton3=0x7f07000e;
         public static final int imageView1=0x7f070002;
         public static final int layout_root=0x7f070001;
-        public static final int love=0x7f07000b;
+        public static final int love=0x7f07000f;
         public static final int password=0x7f070004;
         public static final int signinButton=0x7f070005;
-        public static final int skip=0x7f07000d;
-        public static final int stationsList=0x7f070006;
+        public static final int skip=0x7f070011;
+        public static final int songList=0x7f07000a;
+        public static final int stationsContainer=0x7f070007;
+        public static final int stationsList=0x7f070008;
         public static final int textView1=0x7f070000;
-        public static final int tired=0x7f07000e;
+        public static final int tired=0x7f070012;
+        public static final int twoLineListItem1=0x7f070006;
         public static final int username=0x7f070003;
     }
     public static final class layout {
@@ -41,7 +45,8 @@ public final class R {
         public static final int about_dialog=0x7f030001;
         public static final int main=0x7f030002;
         public static final int signin=0x7f030003;
-        public static final int stations=0x7f030004;
+        public static final int song_item=0x7f030004;
+        public static final int stations=0x7f030005;
     }
     public static final class menu {
         public static final int settings=0x7f060000;
